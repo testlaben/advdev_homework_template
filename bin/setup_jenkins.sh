aefe2266589ec70e0402c09c97a6fff851e3b2fd
Jørgen Dahle Næss <jorgen.ness@gmail.com>
2019-07-19 11:02:05 -0400

    remove create project

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,7 +13,6 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-oc new-project ${GUID}-jenkins --display-name "${GUID} Jenkins instance"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
 oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m
 # Create custom agent container image with skopeo
